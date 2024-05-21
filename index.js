0bffe4078b15ac2e612862db718484d13d9ded20
mdadnanshafiq <adnanbgic2023@gmail.com>
2024-05-21 09:54:21 +0600

    connected with client

--- index.js
+++ index.js
@@ -23,12 +23,24 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+
+    const menuCollection = client.db("bistroDB").collection("menu");
+    const reviewsCollection = client.db("bistroDB").collection("reviews");
+
+    app.get("/menu", async (req, res) => {
+      const result = await menuCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/reviews", async (req, res) => {
+      const result = await reviewsCollection.find().toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // await client.db("admin").command({ ping: 1 });
+    console.log("Connected!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -41,5 +53,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`PORT: ${port}`);
+  //   console.log(`PORT: ${port}`);
 });
