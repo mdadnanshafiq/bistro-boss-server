72b7405ca8c9ac2a9760d278d92fc720508aefff
mdadnanshafiq <adnanbgic2023@gmail.com>
2024-05-23 08:02:26 +0600

    addtocart api added

--- index.js
+++ index.js
@@ -27,9 +27,21 @@ async function run() {
 
     const menuCollection = client.db("bistroDB").collection("menu");
     const reviewsCollection = client.db("bistroDB").collection("reviews");
+    const cartsCollection = client.db("bistroDB").collection("carts");
 
     app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/carts", async (req, res) => {
+      const result = await cartsCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/carts", async (req, res) => {
+      const cartItem = req.body;
+      const result = await cartsCollection.insertOne(cartItem);
       res.send(result);
     });
 
